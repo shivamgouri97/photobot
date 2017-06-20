88e27403bec8b400e22d762ae47b8ecd9977e17e
shivam gouri <shivamgouri1997@gmail.com>
2017-06-20 09:24:47 +0530

    like a post

--- main.py
+++ main.py
@@ -115,3 +115,18 @@ def get_like_list(insta_username):
       print 'No user has liked the post yet!'
   else:
       print 'Status code other than 200 received!'
+
+#like a post
+def like_a_post(insta_username):
+  media_id = get_post_id(insta_username)
+
+  request_url = (BASE_URL + '/media/%s/likes') % (media_id)
+
+  payload = {"access_token": ACCESS_TOKEN}
+
+  print 'Liking the post: %s' % (request_url)
+  post_a_like = requests.post(request_url, payload).json()
+  if post_a_like['meta']['code'] == 200:
+    print 'Like was successful!'
+  else:
+    print 'Your like was unsuccessful. Try again!'

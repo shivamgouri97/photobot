581707d8eed2ed0f3b731866ddb8f92ec802d471
shivam gouri <shivamgouri1997@gmail.com>
2017-06-20 09:06:47 +0530

    get your users post

--- we.py
+++ we.py
@@ -4,24 +4,6 @@
 
 
 
-#get users post
-def get_users_post(insta_username):
-  user_id = get_user_id(insta_username)
-  request_url = (BASE_URL + '/users/%s/media/recent?access_token=%s') % (user_id, ACCESS_TOKEN)
-  print 'Requesting media for: %s' % (request_url)
-  recent_post = requests.get(request_url).json()
-  if recent_post['meta']['code'] == 200:
-    if len(recent_post['data']):
-      image_name = recent_post['data'][0]['id'] + ".jpeg"
-      image_url = recent_post['data'][0]['images']['standard_resolution']['url']
-      urllib.urlretrieve(image_url, image_name)
-
-    else:
-      print "There is no recent post!"
-  else:
-    print "Status code other than 200 received!"
-
-  return None
 
 #get post id
 

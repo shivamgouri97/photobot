6e6e698c5c2d34f2aa60b56b1887efb082aec583
shivam gouri <shivamgouri1997@gmail.com>
2017-06-20 09:03:23 +0530

    get user info by user id

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import urllib
 import requests
 from key import ACCESS_TOKEN
 BASE_URL="http://api.instagram.com/v1"
+
+#function to get self info
 def self_info():
   request_url = (BASE_URL + '/users/self/?access_token=%s') % (ACCESS_TOKEN)
   print 'Requesting info for:' + request_url
@@ -10,8 +13,7 @@ def self_info():
   print 'People I Follow: %s\n' % (my_info['data']['counts']['follows'])
   print 'No. of posts: %s\n' % (my_info['data']['counts']['media'])
 
-#getting self information
-self_info()
+
 
 #getting user id by username
 def get_user_id(insta_username):
@@ -19,14 +21,29 @@ def get_user_id(insta_username):
   print'Requesting info for:' + request_url
 
   search_results = requests.get(request_url).json()
-
+# if search successful
   if search_results['meta']['code'] == 200:
     if len(search_results['data']):
       return search_results['data'][0]['id']
+#if search not succesful
     else:
       print 'User does not exist!'
   else:
     print 'Status code other than 200 was received!'
+    exit()
 
-  return None
+
+# getting user info by using user id'''
+def user_info(insta_username):
+  user_id = get_user_id(insta_username)
+  request_url = (BASE_URL + '/users/%s/?access_token=%s') % (user_id, ACCESS_TOKEN)
+  print 'Requesting info for:' + request_url
+  user_info = requests.get(request_url).json()
+
+  print '%s info is:\n' % (insta_username)
+  print user_info
+  print '%s Followers: %s\n' % (insta_username, user_info['data']['counts']['followed_by'])
+  print 'People %s Follow: %s\n' % (insta_username, user_info['data']['counts']['follows'])
+  print 'No. of posts: %s\n' % (user_info['data']['counts']['media'])
+
 
---
+++ we.py
@@ -0,0 +1,73 @@
+
+#get own posts
+def get_own_post():
+  request_url = (BASE_URL + '/users/self/media/recent?access_token=%s') % (ACCESS_TOKEN)
+  print 'Requesting media for: %s' % (request_url)
+
+  recent_post = requests.get(request_url).json()
+  if recent_post['meta']['code'] == 200:
+    if len(recent_post['data']):
+      #download image
+      image_name = recent_post['data'][0]['id'] + ".jpeg"
+      image_url = recent_post['data'][0]['images']['standard_resolution']['url']
+      urllib.urlretrieve(image_url, image_name)
+
+    else:
+      print "There is no recent post!"
+  else:
+    print "Status code other than 200 received!"
+
+  return None
+
+
+#get users post
+def get_users_post(insta_username):
+  user_id = get_user_id(insta_username)
+  request_url = (BASE_URL + '/users/%s/media/recent?access_token=%s') % (user_id, ACCESS_TOKEN)
+  print 'Requesting media for: %s' % (request_url)
+  recent_post = requests.get(request_url).json()
+  if recent_post['meta']['code'] == 200:
+    if len(recent_post['data']):
+      image_name = recent_post['data'][0]['id'] + ".jpeg"
+      image_url = recent_post['data'][0]['images']['standard_resolution']['url']
+      urllib.urlretrieve(image_url, image_name)
+
+    else:
+      print "There is no recent post!"
+  else:
+    print "Status code other than 200 received!"
+
+  return None
+
+#get post id
+
+def get_post_id(insta_username):
+  user_id = get_user_id(insta_username)
+  request_url = (BASE_URL + 'users/%s/media/recent/?access_token=%s') % (user_id, ACCESS_TOKEN)
+  print 'Requesting data for : %s' % (request_url)
+  user_media = requests.get(request_url).json()
+
+  if user_media['meta']['code'] == 200:
+    if len(user_media['data']):
+      return user_media['data'][0]['id']
+    else:
+      print 'Post does not exist!'
+  else:
+    print 'Status code other than 200 received!'
+  return None
+
+
+#get liked list
+def get_like_list(insta_username):
+  post_id = get_post_id(insta_username)
+
+  request_url = (BASE_URL + '/media/%s/likes?access_token=%s') % (post_id, ACCESS_TOKEN)
+  likes_info = requests.get(request_url).json()
+  if likes_info['meta']['code'] == 200:
+    if len(likes_info['data']):
+      for x in range(0, len(likes_info['data'])):
+        print likes_info['data'][x]['username']
+    else:
+      print 'No user has liked the post yet!'
+  else:
+      print 'Status code other than 200 received!'

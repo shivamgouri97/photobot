f5f1b7b5f881f3a0a52792573456971aab561752
shivam gouri <shivamgouri1997@gmail.com>
2017-06-20 09:07:23 +0530

    get post ID

--- we.py
+++ we.py
@@ -5,22 +5,6 @@
 
 
 
-#get post id
-
-def get_post_id(insta_username):
-  user_id = get_user_id(insta_username)
-  request_url = (BASE_URL + 'users/%s/media/recent/?access_token=%s') % (user_id, ACCESS_TOKEN)
-  print 'Requesting data for : %s' % (request_url)
-  user_media = requests.get(request_url).json()
-
-  if user_media['meta']['code'] == 200:
-    if len(user_media['data']):
-      return user_media['data'][0]['id']
-    else:
-      print 'Post does not exist!'
-  else:
-    print 'Status code other than 200 received!'
-  return None
 
 
 #get liked list
